a410406638a3f891a3b2feb6c7f6b68ed2d652ef
John Costanzo <John.Costanzo@dowjones.com>
2020-12-06 08:36:00 -0500

    Copied a slew of files from the WayBackMachine

---
+++ js/readtheorg.js
@@ -0,0 +1,121 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+$(function() {
+    $('.note').before("<p class='admonition-title note'>Note</p>");
+    $('.seealso').before("<p class='admonition-title seealso'>See also</p>");
+    $('.warning').before("<p class='admonition-title warning'>Warning</p>");
+    $('.caution').before("<p class='admonition-title caution'>Caution</p>");
+    $('.attention').before("<p class='admonition-title attention'>Attention</p>");
+    $('.tip').before("<p class='admonition-title tip'>Tip</p>");
+    $('.important').before("<p class='admonition-title important'>Important</p>");
+    $('.hint').before("<p class='admonition-title hint'>Hint</p>");
+    $('.error').before("<p class='admonition-title error'>Error</p>");
+    $('.danger').before("<p class='admonition-title danger'>Danger</p>");
+});
+
+$( document ).ready(function() {
+
+    // Shift nav in mobile when clicking the menu.
+    $(document).on('click', "[data-toggle='wy-nav-top']", function() {
+      $("[data-toggle='wy-nav-shift']").toggleClass("shift");
+      $("[data-toggle='rst-versions']").toggleClass("shift");
+    });
+    // Close menu when you click a link.
+    $(document).on('click', ".wy-menu-vertical .current ul li a", function() {
+      $("[data-toggle='wy-nav-shift']").removeClass("shift");
+      $("[data-toggle='rst-versions']").toggleClass("shift");
+    });
+    $(document).on('click', "[data-toggle='rst-current-version']", function() {
+      $("[data-toggle='rst-versions']").toggleClass("shift-up");
+    });
+    // Make tables responsive
+    $("table.docutils:not(.field-list)").wrap("<div class='wy-table-responsive'></div>");
+});
+
+$( document ).ready(function() {
+    $('#text-table-of-contents ul').first().addClass('nav');
+                                        // ScrollSpy also requires that we use
+                                        // a Bootstrap nav component.
+    $('body').scrollspy({target: '#text-table-of-contents'});
+
+    // DON'T add sticky table headers (Fix issue #69?)
+    // $('table').stickyTableHeaders();
+
+    // set the height of tableOfContents
+    var $postamble = $('#postamble');
+    var $tableOfContents = $('#table-of-contents');
+    $tableOfContents.css({paddingBottom: $postamble.outerHeight()});
+
+    // add TOC button
+    var toggleSidebar = $('<div id="toggle-sidebar"><a href="#table-of-contents"><h2>Table of Contents</h2></a></div>');
+    $('#content').prepend(toggleSidebar);
+
+    // add close button when sidebar showed in mobile screen
+    var closeBtn = $('<a class="close-sidebar" href="#">Close</a>');
+    var tocTitle = $('#table-of-contents').find('h2');
+    tocTitle.append(closeBtn);
+});
+
+window.SphinxRtdTheme = (function (jquery) {
+    var stickyNav = (function () {
+        var navBar,
+            win,
+            stickyNavCssClass = 'stickynav',
+            applyStickNav = function () {
+                if (navBar.height() <= win.height()) {
+                    navBar.addClass(stickyNavCssClass);
+                } else {
+                    navBar.removeClass(stickyNavCssClass);
+                }
+            },
+            enable = function () {
+                applyStickNav();
+                win.on('resize', applyStickNav);
+            },
+            init = function () {
+                navBar = jquery('nav.wy-nav-side:first');
+                win    = jquery(window);
+            };
+        jquery(init);
+        return {
+            enable : enable
+        };
+    }());
+    return {
+        StickyNav : stickyNav
+    };
+}($));
+
+
+}
+/*
+     FILE ARCHIVED ON 23:12:09 Jun 02, 2020 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 07:14:46 Nov 23, 2020.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  exclusion.robots.policy: 0.229
+  CDXLines.iter: 23.021 (3)
+  load_resource: 355.721
+  captures_list: 320.704
+  exclusion.robots: 0.251
+  RedisCDXSource: 3.136
+  PetaboxLoader3.resolve: 205.08
+  PetaboxLoader3.datanode: 421.291 (4)
+  esindex: 0.011
+  LoadShardBlock: 289.998 (3)
+*/
