7fa73803777e93fe5871e8ccac8218344134cf5b
John Costanzo <jncostanzo@gmail.com>
2023-09-08 16:55:49 -0400

    Objectified CircularTimer and support passing in initial time.

--- js/circular-timer-using-dojo.js
+++ js/circular-timer-using-dojo.js
@@ -1,148 +1,175 @@
-// File last changed: <2023-09-08 Fri 16:27:56>
+// File last changed: <2023-09-08 Fri 16:55:38>
 
-    const zeroPad = (num, places) => String(num).padStart(places, '0');
+const zeroPad = ( num, places ) => String( num ).padStart( places, '0' );
 
-function createTimer( timerContainer ) {
-    require([
-        "dojo/dom", "dojo/dom-construct", "dojo/on", "dojo/ready",
-    ], function (dom, domConstruct, on, ready) {
-        ready(function () {
-	    
-	    const circularTimer = dom.byId( timerContainer );
-	    if( !circularTimer ) {
-		console.log( "createTimer: cannot locate " + timerContainer );
-		return;
-	    }
+class CircularTimer {
 
-	    // Create an SVG element
-	    var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-	    svg.setAttribute("width", "100");
-	    svg.setAttribute("height", "100");
+    /**
+     * Construct a new CircularTimer, optionally configure to play sounds upon completion.
+     *
+     * @param {number} initial_time - The number of seconds the timer is initialized with each time it is run.
+     * @param {number} alert_time   - The number of seconds remaining in the countdown that the timer should go into alert mode.
+     * @param {map of arrays} sounds - A HashMap keyed on integers, which specify number of seconds remaining in the countdown, 
+     *                                 and values of either a string or an  arrary of strings that specify file names to play at those times.
+     *
+     * @example
+     * // Constructs a new ( silent ) CircularTimer initialized to 10 seconds.
+     * let ct1 = new CircularTimer( 10 )
+     *
+     * @example
+     * // Constructs a new CircularTimer initialized to 30 seconds that
+     * // plays two sounds with 10 seconds remaining and another at countdown termination.
+     * let s = new Object( );
+     * s[ 10 ] = [ 'fileA', 'fileB' ];
+     * s[ 0 ] = 'fileC';
+     * let ct2 = new CircularTimer( 30, s )
+     *
+     * @returns {CircularTimer} Returns a new CircularTimer embedded in the page at the point of call.
+     */
+    constructor( timerContainer, initialTime, alertTime, sounds ) {
+	var circularTimer;
+	var initialSeconds = initialTime % 60;
+	var initialMinutes = Math.floor( initialTime / 60 );
 
-	    // Create a circle
-	    var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-	    circle.setAttribute("cx", "50");
-	    circle.setAttribute("cy", "50");
-	    circle.setAttribute("r", "45");
-	    circle.setAttribute("fill", "lightblue");
-	    circle.classList.add( "timer-arc");
+	require( [
+            "dojo/dom", "dojo/dom-construct", "dojo/on", "dojo/ready",
+	], function ( dom, domConstruct, on, ready ) {
+            ready( function ( ) {
 
-	    // Create text
-	    var text = document.createElementNS("http://www.w3.org/2000/svg", "text");
-	    text.setAttribute("font-size", "32");
-	    const textX = parseInt( circle.getAttribute("cx") );
-	    const textY = parseInt( circle.getAttribute("cy") ) + ( text.getAttribute("font-size")/2 );
-
-	    text.setAttribute("x", textX );
-	    text.setAttribute("y", textY);
-	    text.setAttribute("text-anchor", "middle");
-	    text.textContent = "";
-
-	    // Append the circle and text to the SVG element
-	    svg.appendChild(circle);
-	    svg.appendChild(text);
-
-	    // Append the SVG element to the circle container
-	    circularTimer.appendChild(svg);
-
-	    if( !svg || !circle ) {
-		console.log( "createTimer: cannot create circle within an SVG element" );
-		return;
-	    }
-
-	    const timerControls = domConstruct.create( "div", {
-	    }, circularTimer );
-
-	    const minutes = domConstruct.create( "input", {
-		type: "number",
-		value: "0",
-		min: "0",
-		innerHTML: "minutes"
-	    }, timerControls );
-
-	    const colon = domConstruct.create( "span", {
-		innerHTML: " : "
-	    }, timerControls );
-	    
-	    const seconds = domConstruct.create( "input", {
-		type: "number",
-		value: "30",
-		min: "0",
-		max: "59",
-		innerHTML: "seconds"
-	    }, timerControls );
-
-	    const startButton = domConstruct.create( "button", {
-		innerHTML: "Start"
-	    }, circularTimer );
-	    var startButtonHandler;
-
-	    const radius = circle.r.baseVal.value;
-	    const circumference = 2 * Math.PI * radius;
-
-	    var totalTime = 0; // in seconds
-	    var remainingTime = 0; // in seconds
-	    var timerInterval;
-
-	    startButtonHandler = on(startButton, "click", startTimer);
-
-	    function commenceTicking() {
-		if (remainingTime <= 0) {
-                    clearInterval(timerInterval);
-		    startButton.innerHTML = "Start";
-		    startButtonHandler.remove();
-		    startButtonHandler = on(startButton, "click", startTimer);
-                    return;
+		const circularTimer = dom.byId( timerContainer );
+		if( !circularTimer ) {
+		    console.log( "createTimer: cannot locate " + timerContainer );
+		    return;
 		}
-		remainingTime--;
-		updateDisplay();
-            }
 
-	    function pauseTimer() {
-		console.log( "pauseTimer" );
-                clearInterval(timerInterval);
-		startButton.innerHTML = "Resume";
-		startButtonHandler.remove();
-		startButtonHandler = on(startButton, "click", resumeTimer);
-	    }
+		// Create an SVG element
+		var svg = document.createElementNS( "http://www.w3.org/2000/svg", "svg" );
+		svg.setAttribute( "width", "100" );
+		svg.setAttribute( "height", "100" );
 
-	    function resumeTimer() {
-		console.log( "resumeTimer" );
-                clearInterval(timerInterval);
-		startButton.innerHTML = "Pause";
-		startButtonHandler.remove();
-		startButtonHandler = on(startButton, "click", pauseTimer);
+		// Create a circle
+		var circle = document.createElementNS( "http://www.w3.org/2000/svg", "circle" );
+		circle.setAttribute( "cx", "50" );
+		circle.setAttribute( "cy", "50" );
+		circle.setAttribute( "r", "45" );
+		circle.setAttribute( "fill", "lightblue" );
+		circle.classList.add( "timer-arc" );
 
-                timerInterval = setInterval(commenceTicking, 1000);
-	    }
+		// Create text
+		var text = document.createElementNS( "http://www.w3.org/2000/svg", "text" );
+		text.setAttribute( "font-size", "32" );
+		const textX = parseInt( circle.getAttribute( "cx" ) );
+		const textY = parseInt( circle.getAttribute( "cy" ) ) + ( text.getAttribute( "font-size" )/2 );
 
-	    function startTimer() {
-                clearInterval(timerInterval);
-		console.log( "startTimer" );
-		startButton.innerHTML = "Pause";
-		startButtonHandler.remove();
-		startButtonHandler = on(startButton, "click", pauseTimer);
+		text.setAttribute( "x", textX );
+		text.setAttribute( "y", textY );
+		text.setAttribute( "text-anchor", "middle" );
+		text.textContent = "";
 
-                totalTime = (parseInt(minutes.value) || 0) * 60 + (parseInt(seconds.value) || 0);
-                remainingTime = totalTime;
+		// Append the circle and text to the SVG element
+		svg.appendChild( circle );
+		svg.appendChild( text );
 
-                updateDisplay();
-                timerInterval = setInterval(commenceTicking, 1000);
-	    }
+		// Append the SVG element to the circle container
+		circularTimer.appendChild( svg );
 
-	    function updateDisplay() {
-                var min = Math.floor(remainingTime / 60);
-                var sec = remainingTime % 60;
-		text.textContent = zeroPad( min, 2 ) + ":" + zeroPad( sec, 2 );
+		if( !svg || !circle ) {
+		    console.log( "createTimer: cannot create circle within an SVG element" );
+		    return;
+		}
+
+		const timerControls = domConstruct.create( "div", {
+		}, circularTimer );
+
+		const minutes = domConstruct.create( "input", {
+		    type: "number",
+		    value: initialMinutes,
+		    min: "0",
+		    innerHTML: "minutes"
+		}, timerControls );
+
+		const colon = domConstruct.create( "span", {
+		    innerHTML: " : "
+		}, timerControls );
+
+		const seconds = domConstruct.create( "input", {
+		    type: "number",
+		    value: initialSeconds,
+		    min: "0",
+		    max: "59",
+		    innerHTML: "seconds"
+		}, timerControls );
+
+		const startButton = domConstruct.create( "button", {
+		    innerHTML: "Start"
+		}, circularTimer );
+		var startButtonHandler;
+
+		const radius = circle.r.baseVal.value;
+		const circumference = 2 * Math.PI * radius;
+
+		var totalTime = 0; // in seconds
+		var remainingTime = 0; // in seconds
+		var timerInterval;
+
+		startButtonHandler = on( startButton, "click", startTimer );
+
+		function commenceTicking( ) {
+		    if ( remainingTime <= 0 ) {
+			clearInterval( timerInterval );
+			startButton.innerHTML = "Start";
+			startButtonHandler.remove( );
+			startButtonHandler = on( startButton, "click", startTimer );
+			return;
+		    }
+		    remainingTime--;
+		    updateDisplay( );
+		}
+
+		function pauseTimer( ) {
+                    clearInterval( timerInterval );
+		    startButton.innerHTML = "Resume";
+		    startButtonHandler.remove( );
+		    startButtonHandler = on( startButton, "click", resumeTimer );
+		}
+
+		function resumeTimer( ) {
+                    clearInterval( timerInterval );
+		    startButton.innerHTML = "Pause";
+		    startButtonHandler.remove( );
+		    startButtonHandler = on( startButton, "click", pauseTimer );
+
+                    timerInterval = setInterval( commenceTicking, 1000 );
+		}
+
+		function startTimer( ) {
+                    clearInterval( timerInterval );
+		    startButton.innerHTML = "Pause";
+		    startButtonHandler.remove( );
+		    startButtonHandler = on( startButton, "click", pauseTimer );
+
+                    totalTime = ( parseInt( minutes.value ) || 0 ) * 60 + ( parseInt( seconds.value ) || 0 );
+                    remainingTime = totalTime;
+
+                    updateDisplay( );
+                    timerInterval = setInterval( commenceTicking, 1000 );
+		}
+
+		function updateDisplay( ) {
+                    var min = Math.floor( remainingTime / 60 );
+                    var sec = remainingTime % 60;
+		    text.textContent = zeroPad( min, 2 ) + ":" + zeroPad( sec, 2 );
 
 
-                // Update the SVG timer arc
-		const fractionRemaining = ( ( remainingTime / totalTime ) * circumference );
-		circle.style.strokeDasharray = fractionRemaining + " " + circumference;
-	    }
-        });
-    });
-}
+                    // Update the SVG timer arc
+		    const fractionRemaining = ( ( remainingTime / totalTime ) * circumference );
+		    circle.style.strokeDasharray = fractionRemaining + " " + circumference;
+		}
+            } );
+	} );
+	return( circularTimer );
+    }
+};
 
 
 // Local Variables:
