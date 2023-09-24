1cdb728a5784851bcf5668be97b5450a3cbe0cfe
John Costanzo <jncostanzo@gmail.com>
2023-09-23 20:48:28 -0400

    Support diacritic-insensitive searching.

--- js/search-recipes.js
+++ js/search-recipes.js
@@ -1,8 +1,8 @@
-const debugSearchRecipes = false;
+var debugSearchRecipes = true;
 
 // Turn off display for element with ID.
 function hideElement( id ) {
-    if( debugSearchRecipes ) console.log( "\t\t\thide element with id=[%s]", id );
+    // if( debugSearchRecipes ) console.log( "\t\t\thide element with id=[%s]", id );
     const el = document.getElementById( id );
     if( el ) {
 	el.style.display = "none";
@@ -18,7 +18,7 @@ function hideElements( id ) {
 
 // Turn on display for element with ID. Use block display.
 function showElement( id ) {
-    if( debugSearchRecipes ) console.log( "\t\t\tshow element with id=[%s]", id );
+    // if( debugSearchRecipes ) console.log( "\t\t\tshow element with id=[%s]", id );
     const el = document.getElementById( id );
     if( el ) {
 	el.style.display = "list-item";
@@ -35,15 +35,17 @@ function showElements( id ) {
 // Return true if all of the terms in the array QUERY
 // are found within the string STR, else false.
 function matchesAll( query, str ) {
-     for( const term of query ) {
+    for( const term of query ) {
 	const regexp = new RegExp( term );
 	if( str.search( regexp ) == -1 ) {
+	    // if( debugSearchRecipes ) console.log( "matchesAll: query=[%s] does NOT match all of [%s]!", query, str );
 	    return( false );
 	}
     }
+    // if( debugSearchRecipes ) console.log( "matchesAll: query=[%s] DOES match all of [%s]!", query, str );
     return( true );
 }
-    
+
 
 // QUERY is an array of strings.
 // INDEX is an array of arrays: [ id, indexString ].
@@ -58,7 +60,7 @@ function searchElements( query, index ) {
     for( const idx of index ) {
 	const id = idx[0];
 	const indexString = idx[1];
-	if( debugSearchRecipes ) console.log( "\tid=[%s]\tindexString=[%s]", id, indexString );
+	// if( debugSearchRecipes ) console.log( "\tid=[%s]\tindexString=[%s]", id, indexString );
 	if( matchesAll( query, indexString ) ) {
 	    showElement( id );
 	} else {
@@ -67,14 +69,27 @@ function searchElements( query, index ) {
     }
 }
 
+function removeDiacritics( string ) {
+    // Return STRING but with all diacritcs removed.
+    return string
+	.normalize( 'NFD' )
+	.replace( /[\u0300-\u036f]/g, '' )
+	.replace( /ß/g, 'ss' )
+	.replace( /æ/g, 'ae' )
+    ;
+}
+
+
 // Assume the keyboard event has already been processed.
 // Use the query in the query box to filter the page according to the recipeIndex
 function keyboardHandler(e) {
-    let query = document.
-	getElementById( "searchBox" ).
-	value.
-	toLowerCase().
-	replace( /[^0-9a-z ]+/g, "" );
+    let query = removeDiacritics( 
+	document.
+	    getElementById( "searchBox" )
+	    .value
+	    .toLowerCase()
+	    .replace( /[^0-9 \p{L}]+/gu, "" )
+    );
     if( debugSearchRecipes ) console.log( "query=[%s]", query );
     searchElements( query.split( " " ), recipeIndexArray );
 }
@@ -97,15 +112,18 @@ document.getElementById( "searchBox" ).
 
 function searchDriver() {
     const queries = [
-	['bre'],
-	['bread'],
-	['break'],
-	['piz'],
-	['nach'],
-	['garbage'],
+	// ['bre'],
+	// ['bread'],
+	// ['break'],
+	// ['piz'],
+	// ['nach'],
+	// ['garbage'],
 
-	['bre', 'piz'],
-	['bre', 'flour'],
+	// ['bre', 'piz'],
+	// ['bre', 'flour'],
+	['jalapeno'],
+	['jalapeñ'],
+	['jalapeño'],
     ];
     for( const q of queries ) {
 	console.log( "query=[%s]", q.join(", ") );
