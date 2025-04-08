3ed9cdf55be6e92cf209aac0539deaea28ecac1d
jncostanzo <jncostanzo@penguin>
2025-04-07 20:47:52 -0400

    Formatted with js-beautify.

--- js/search-recipes.js
+++ js/search-recipes.js
@@ -1,51 +1,51 @@
-const searchRecipesVersion = "Friday, 2025-03-28 @ 14:47:05";
+const searchRecipesVersion = "Monday, 2025-04-07 @ 20:47:20";
 
 var debugSearchRecipes = false;
 
 // Turn off display for element with ID.
-function hideElement( id ) {
+function hideElement(id) {
     // if( debugSearchRecipes ) console.log( "\t\t\thide element with id=[%s]", id );
-    const el = document.getElementById( id );
-    if( el ) {
-	el.style.display = "none";
+    const el = document.getElementById(id);
+    if (el) {
+        el.style.display = "none";
     } else {
-	console.log( "no element with id=[%s]", id );
+        console.log("no element with id=[%s]", id);
     }
 }
 
 // Turn off display for element with ID and its related ids.
-function hideElements( id ) {
-    hideElement( id );
+function hideElements(id) {
+    hideElement(id);
 }
 
 // Turn on display for element with ID. Use block display.
-function showElement( id ) {
+function showElement(id) {
     // if( debugSearchRecipes ) console.log( "\t\t\tshow element with id=[%s]", id );
-    const el = document.getElementById( id );
-    if( el ) {
-	el.style.display = "list-item";
+    const el = document.getElementById(id);
+    if (el) {
+        el.style.display = "list-item";
     } else {
-	console.log( "no element with id=[%s]", id );
+        console.log("no element with id=[%s]", id);
     }
 }
 
 // Turn on display for element with ID and its related ids.
-function showElements( id ) {
-    showElement( id );
+function showElements(id) {
+    showElement(id);
 }
 
 // Return true if all of the terms in the array QUERY
 // are found within the string STR, else false.
-function matchesAll( query, str ) {
-    for( const term of query ) {
-	const regexp = new RegExp( term );
-	if( str.search( regexp ) == -1 ) {
-	    // if( debugSearchRecipes ) console.log( "matchesAll: query=[%s] does NOT match all of [%s]!", query, str );
-	    return( false );
-	}
+function matchesAll(query, str) {
+    for (const term of query) {
+        const regexp = new RegExp(term);
+        if (str.search(regexp) == -1) {
+            // if( debugSearchRecipes ) console.log( "matchesAll: query=[%s] does NOT match all of [%s]!", query, str );
+            return (false);
+        }
     }
     // if( debugSearchRecipes ) console.log( "matchesAll: query=[%s] DOES match all of [%s]!", query, str );
-    return( true );
+    return (true);
 }
 
 
@@ -54,50 +54,48 @@ function matchesAll( query, str ) {
 // Call hideElement for all elements with an id from INDEX where *all* of the strings
 // in the QUERY array may be found as regular expressions in the corresponding indexString.
 // Else call showElement for those ids.
-function searchElements( query, index ) {
+function searchElements(query, index) {
     let result = []
-    if( debugSearchRecipes ) console.log( "searchElements: index=[%s]\tquery=[%s]", index.join(", "), query.join(", ") );
+    if (debugSearchRecipes) console.log("searchElements: index=[%s]\tquery=[%s]", index.join(", "), query.join(", "));
 
     indexLoop:
-    for( const idx of index ) {
-	const id = idx[0];
-	const indexString = idx[1];
-	// if( debugSearchRecipes ) console.log( "\tid=[%s]\tindexString=[%s]", id, indexString );
-	if( matchesAll( query, indexString ) ) {
-	    showElement( id );
-	} else {
-	    hideElement( id );
-	}
-    }
+        for (const idx of index) {
+            const id = idx[0];
+            const indexString = idx[1];
+            // if( debugSearchRecipes ) console.log( "\tid=[%s]\tindexString=[%s]", id, indexString );
+            if (matchesAll(query, indexString)) {
+                showElement(id);
+            } else {
+                hideElement(id);
+            }
+        }
 }
 
-function removeDiacritics( string ) {
+function removeDiacritics(string) {
     // Return STRING but with all diacritcs removed.
     return string
-	.normalize( 'NFD' )
-	.replace( /[\u0300-\u036f]/g, '' )
-	.replace( /ß/g, 'ss' )
-	.replace( /æ/g, 'ae' )
-    ;
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '')
+        .replace(/ß/g, 'ss')
+        .replace(/æ/g, 'ae');
 }
 
 
 // Assume the keyboard event has already been processed.
 // Use the query in the query box to filter the page according to the recipeIndex
-function searchKeyboardHandler( ) {
+function searchKeyboardHandler() {
     let query = removeDiacritics(
-	document.
-	    getElementById( "searchBox" )
-	    .value
-	    .toLowerCase()
-	    .replace( /[^0-9 \p{L}]+/gu, "" )
+        document.getElementById("searchBox")
+        .value
+        .toLowerCase()
+        .replace(/[^0-9 \p{L}]+/gu, "")
     );
-    if( debugSearchRecipes ) console.log( "query=[%s]", query );
-    searchElements( query.split( " " ), recipeIndexArray );
+    if (debugSearchRecipes) console.log("query=[%s]", query);
+    searchElements(query.split(" "), recipeIndexArray);
 }
 
-document.getElementById( "searchBox" ).
-    addEventListener( 'keyup', searchKeyboardHandler );
+document.getElementById("searchBox").
+addEventListener('keyup', searchKeyboardHandler);
 
 //==================================================================
 // Test Functions
@@ -114,22 +112,22 @@ document.getElementById( "searchBox" ).
 
 function searchDriver() {
     const queries = [
-	// ['bre'],
-	// ['bread'],
-	// ['break'],
-	// ['piz'],
-	// ['nach'],
-	// ['garbage'],
+        // ['bre'],
+        // ['bread'],
+        // ['break'],
+        // ['piz'],
+        // ['nach'],
+        // ['garbage'],
 
-	// ['bre', 'piz'],
-	// ['bre', 'flour'],
-	['jalapeno'],
-	['jalapeñ'],
-	['jalapeño'],
+        // ['bre', 'piz'],
+        // ['bre', 'flour'],
+        ['jalapeno'],
+        ['jalapeñ'],
+        ['jalapeño'],
     ];
-    for( const q of queries ) {
-	console.log( "query=[%s]", q.join(", ") );
-	searchElements( q, recipeIndexArray );
+    for (const q of queries) {
+        console.log("query=[%s]", q.join(", "));
+        searchElements(q, recipeIndexArray);
     }
 }
 
