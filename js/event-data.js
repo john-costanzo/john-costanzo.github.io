45da648313800c74afaf825e08795dc3e2daf3be
John Costanzo <jncostanzo@gmail.com>
2025-06-12 10:29:35 -0400

    local event data update Thursday, 2025-06-12 10:29:35

--- js/event-data.js
+++ js/event-data.js
@@ -1,5 +1,5 @@
-const eventDataVersion = "Thursday, 2025-06-12 10:29:26";
-console.log("eventDataVersion = Thursday, 2025-06-12 10:29:26")
+const eventDataVersion = "Thursday, 2025-06-12 10:29:35";
+console.log("eventDataVersion = Thursday, 2025-06-12 10:29:35")
 const events = [
     {
         url: "https://pointlookoutvineyards.com/whats-happening",
