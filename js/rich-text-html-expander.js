140bedd9607c74f60d1feb63e0a90c068be3abf2
John Costanzo <jncostanzo@gmail.com>
2026-08-05 11:51:12 -0400

    Removed unused and nuisance expansions.

--- js/rich-text-html-expander.js
+++ js/rich-text-html-expander.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Rich Text HTML Expander
 // @namespace    http://tampermonkey.net/
-// @version      2026-07-29_20-08
+// @version      2026-08-05_07-24
 // @description  Intercepts typing and inserts an expansion text via native HTML paste handling
 // @match        *://*/*
 // @grant        none
@@ -13,7 +13,6 @@
     const EXPANSIONS = {
         "--": "—",
         "->": "→",
-        "...": "…",
         "25bmc": "25 Bay Magnolia Ct.",
         ";-": "<hr><br>",
         ";1d": "${DATE \"DDDD, MMMM D\" +1D}",
@@ -39,6 +38,7 @@
         ";checkmark": "<span style=\"font-size: x-large;\">✅</span>",
         ";confused": "<img src=\"https://em-content.zobj.net/source/skype/289/confused-face_1f615.png\" width=\"42\" height=\"42\" alt=\"Confused Face on Skype Emoticons 1.2\"/>",
         ";cow": "<img src=\"https://em-content.zobj.net/source/microsoft-teams/337/cow-face_1f42e.png\" width=\"66\" height=\"66\" alt=\"Cow Face on Microsoft Teams 1.0\"/>",
+        ";cmt": "committee",
         ";cp": "Carriage Park",
         ";crazy": "<img src=\"https://em-content.zobj.net/source/noto-emoji-animations/344/winking-face-with-tongue_1f61c.gif\" width=\"42\" height=\"42\" alt=\"Winking Face with Tongue on Noto Color Emoji, Animated 14.0\"/>",
         ";cry": "<img src=\"https://em-content.zobj.net/source/skype/289/crying-face_1f622.png\" width=\"42\" height=\"42 alt=\"Crying Face on Skype Emoticons 1.2\"/>",
@@ -50,7 +50,6 @@
         ";dmm": "Request to post draft ${DATE \"MMMM YYYY\"}) CPHA Board meeting minutes<p><p>Attached are draft CPHA Board minutes for its ${DATE \"MMMM YYYY\") meeting.<p><p>Please post them to the CPHA website.<p><p>​Thanks.<p>",
         ";dtime": "${DATE \"DDDD, MMMM D YYYY, h:mm:ss a\"}",
         ";emb": "<img src=\"https://www.kindpng.com/picc/m/47-476214_embarrassed-emoji-png-download-embarrassment-embarrassed-emoji-gif.png\" width=\"42\" height=\"42\" alt=\"Embarrassed Face\"/>",
-        ";enl": "Using kdenlive, show how to ",
         ";es": "executive session",
         ";explode": "<img src=\"https://em-content.zobj.net/source/noto-emoji-animations/344/exploding-head_1f92f.gif\" width=\"42\" height=\"42\" alt=\"Exploding Head on Noto Color Emoji, Animated 14.0\"/>",
         ";eyeroll": "<img src=\"https://gifdb.com/images/high/eye-roll-emoji-singing-cartoon-animation-ixx9s6eqp7vh86cz.gif\" width=\"42\" height=\"42\"  alt=\"Eye Roll Emoji GIFs | GIFDB.com\"/>",
@@ -83,13 +82,13 @@
         ";memo": "📝",
         ";moo": "<img src=\"https://em-content.zobj.net/source/microsoft-teams/337/cow-face_1f42e.png\" width=\"66\" height=\"66\" alt=\"Cow Face on Microsoft Teams 1.0\"/>",
         ";mycell": "8285134093",
-        ";nafy": "<i>(No action for you.)</i><p>",
+        ";nafy": "<i style=\"color: grey; font-size: medium;\">(No action for you.)</i><p>",
         ";notes": "<img src=\"https://em-content.zobj.net/source/noto-emoji-animations/344/musical-notes_1f3b6.gif\" width=\"42\" height=\"42 alt=\"Musical Notes on Noto Color Emoji, Animated 14.0\"/>",
         ";ontarget": "<span style=\"font-size: x-large;\">🎯</span>",
         ";pbt": "Pickleball for ${DATE \"DDDD, MMMM D, YYYY\"}",
         ";pc1": "<style type=\"text/css\">div, li, p {font-size:large;}</style>Henderson County Democrats could use your help!<p>Thank you for volunteering to help write and send postcards. This is an important activity to help build momentum for future Democratic victories.<p><p>Over the next couple of weeks, we\'d like you to write about 25 postcards welcoming newly registered voters and encouraging them to be active with the Henderson County Democrats. I would provide the postcards, addresses, and a script. You would write and mail postcards no later than the end of <span style=\"font-weight: bold;\">${DATE \"MMMM\" +2W}</span>. It would be great if you could supply your own stamps. Otherwise, we may have some available at Democratic headquarters… or you can purchase them yourself and request reimbursement.<p><p><center><span style=\"font-size: x-large; font-weight: bold;\">👉&nbsp;Can we count on you to send out some postcards now?&nbsp;👈</span></center><p>Please let me know your decision by end of day ${DATE \"DDDD, MMMM D, YYYY\" +3D}.<p><p>Oh, and if you would like to be removed from this email list, please let me know and I\'d be happy to make that happen.<p><p>Either way, thank you for your support.<p><p><span style=\"color:blue;\">John Costanzo<br>Henderson County Democrats Postcard Drive Coordinator</span>",
         ";pc2": "<style type=\"text/css\">div, li, p {font-size:large;}</style><style type=\"text/css\">div, li, p {font-size:large;}</style><span style=\"color:rgb(0,0,0);font-family:Georgia,serif\">I\'m checking whether this is something you\'d be interested in doing.<p>If not, I\'d be happy to remove your name from the list.<p><p>Thanks.<p><p><font color=\\\"#0000ff\\\">— jc</font><p></span>",
-        ";pca": "<style type=\"text/css\">div, li, p {font-size:large;}</style>​Henderson County Democrats: postcard instructions<br class=\"gmail_default\" style=\"font-size:large;color:rgb(0,0,0)\"><div>Thanks again for volunteering to help motivate our fellow citizens to become more politically active. Here\'s what you\'ll need:</div> <ol> <li style=\"margin-left:15px\">A packet containing your postcards is waiting for you at the office of the Henderson County Democrats at their (new) office location at <a href=\"https://www.google.com/maps/place/2024+Asheville+Hwy+suite+f,+Hendersonville,+NC+28791/@35.3380422,-82.4735369,17z/data=!3m1!4b1!4m6!3m5!1s0x8859e9f8c3f773eb:0x1b647bb6746bbb4c!8m2!3d35.3380422!4d-82.4735369!16s%2Fg%2F11rnfb0tmf!5m1!1e1?entry=ttu&amp;g_ep=EgoyMDI1MTAyMC4wIKXMDSoASAFQAw%3D%3D\" target=\"_blank\" data-saferedirecturl=\"https://www.google.com/url?q=https://www.google.com/maps/place/2024%2BAsheville%2BHwy%2Bsuite%2Bf,%2BHendersonville,%2BNC%2B28791/@35.3380422,-82.4735369,17z/data%3D!3m1!4b1!4m6!3m5!1s0x8859e9f8c3f773eb:0x1b647bb6746bbb4c!8m2!3d35.3380422!4d-82.4735369!16s%252Fg%252F11rnfb0tmf!5m1!1e1?entry%3Dttu%26g_ep%3DEgoyMDI1MTAyMC4wIKXMDSoASAFQAw%253D%253D&amp;source=gmail&amp;ust=1769258547192000&amp;usg=AOvVaw1AK3NE7fV8_XbjRRIWZR_4\">2024 Asheville Highway, Suite F, Hendersonville, NC 28791</a>. <ol type=\"a\"> <li style=\"margin-left:15px\">The office is open from 11am to 2pm, Monday through Saturday. <i>Although if there is inclement weather, you may want to check their website (<a href=\"http://myhcdp.com/\" target=\"_blank\">myhcdp.com</a>) before you head out.</i></li> <li style=\"margin-left:15px\">A packet with your name on it should be waiting for you on a table behind the partition in back of the desk on your left as you enter the office.</li> <li style=\"margin-left:15px\">If you cannot pick up your packet during office hours or have any issues, please let me know, and we\'ll make other arrangements.</li> </ol> </li> <li style=\"margin-left:15px\">Attached to this note are the addresses you will use to address the cards. Note: names shaded in <span style=\"background-color:rgb(182,215,168)\">green</span> belong to a single address and therefore should receive a single postcard.</li> <li style=\"margin-left:15px\"> Please use the following message on your postcards: <div style=\"margin-bottom:10px;padding:10px;background-color:rgb(255,248,220);border-left:6px solid rgb(255,228,102)\"> Thanks for registering as a Democrat. Henderson County Democrats have many fun and1 effective gatherings for you and your family in the works as you can see from our website <a href=\"http://myhcdp.com/\" target=\"_blank\">myhcdp.com</a>.<br><br>We\'d love to see you at an event!  </div> </li> <li style=\"margin-left:15px\">You can sign the note with your first name and last initial <i>(optional)</i>.</li> <li style=\"margin-left:15px\">As I mentioned previously, it would be great if you could provide postcard stamps. But if not, the office may have some, or you could be reimbursed after you purchase some. Ask the person at the desk.</li> <li style=\"margin-left:15px\">Please mail all postcards no later than the <b>end of ${DATE \"MMMM\" +2W}</b>.</li> <li style=\"margin-left:15px\">Finally, please email me when you are finished.</li> </ol> <p>Thanks for all your efforts!</p> <div dir=\"ltr\" class=\"gmail_signature\" data-lt-sig=\"1\">  <span style=\"color:blue;\">John Costanzo <br>Henderson County Democrats Postcard Drive Coordinator</span>  </div> <br clear=\"all\">",
+        ";pca": "<style type=\"text/css\">div, li, p {font-size:large;}</style>​Henderson County Democrats: postcard instructions<br class=\"gmail_default\" style=\"font-size:large;color:rgb(0,0,0)\"><div>Thanks again for volunteering to help motivate our fellow citizens to become more politically active. Here\'s what you\'ll need:</div> <ol> <li style=\"margin-left:15px\">A packet containing your postcards is waiting for you at the office of the Henderson County Democrats at their (new) office location at <a href=\"https://www.google.com/maps/place/2024+Asheville+Hwy+suite+f,+Hendersonville,+NC+28791/@35.3380422,-82.4735369,17z/data=!3m1!4b1!4m6!3m5!1s0x8859e9f8c3f773eb:0x1b647bb6746bbb4c!8m2!3d35.3380422!4d-82.4735369!16s%2Fg%2F11rnfb0tmf!5m1!1e1?entry=ttu&amp;g_ep=EgoyMDI1MTAyMC4wIKXMDSoASAFQAw%3D%3D\" target=\"_blank\" data-saferedirecturl=\"https://www.google.com/url?q=https://www.google.com/maps/place/2024%2BAsheville%2BHwy%2Bsuite%2Bf,%2BHendersonville,%2BNC%2B28791/@35.3380422,-82.4735369,17z/data%3D!3m1!4b1!4m6!3m5!1s0x8859e9f8c3f773eb:0x1b647bb6746bbb4c!8m2!3d35.3380422!4d-82.4735369!16s%252Fg%252F11rnfb0tmf!5m1!1e1?entry%3Dttu%26g_ep%3DEgoyMDI1MTAyMC4wIKXMDSoASAFQAw%253D%253D&amp;source=gmail&amp;ust=1769258547192000&amp;usg=AOvVaw1AK3NE7fV8_XbjRRIWZR_4\">2024 Asheville Highway, Suite F, Hendersonville, NC 28791</a>. <ol type=\"a\"> <li style=\"margin-left:15px\">The office is open from 11am to 2pm, Monday through Saturday. <i>Although if there is inclement weather, you may want to check their website (<a href=\"http://myhcdp.com/\" target=\"_blank\">myhcdp.com</a>) before you head out.</i></li> <li style=\"margin-left:15px\">A packet with your name on it should be waiting for you on a table behind the partition in back of the desk on your left as you enter the office.</li> <li style=\"margin-left:15px\">If you cannot pick up your packet during office hours or have any issues, please let me know, and we\'ll make other arrangements.</li> </ol> </li> <li style=\"margin-left:15px\">Attached to this note are the addresses you will use to address the cards. Note: names shaded in <span style=\"background-color:rgb(182,215,168)\">green</span> belong to a single address and therefore should receive a single postcard.</li> <li style=\"margin-left:15px\"> Please use the following message on your postcards: <div style=\"margin-bottom:10px;padding:10px;background-color:rgb(255,248,220);border-left:6px solid rgb(255,228,102)\">Thanks for registering as a Democrat. Henderson County Democrats have many fun and effective gatherings in the works for you and your family, as you can see from our website <a href=\"http://myhcdp.com/\" target=\"_blank\">myhcdp.com</a>.<br><br>We\'d love to see you at an event!  </div> </li> <li style=\"margin-left:15px\">You can sign the note with your first name and last initial <i>(optional)</i>.</li> <li style=\"margin-left:15px\">As I mentioned previously, it would be great if you could provide postcard stamps. But if not, the office may have some, or you could be reimbursed after you purchase some. Ask the person at the desk.</li> <li style=\"margin-left:15px\">Please mail all postcards no later than the <b>end of ${DATE \"MMMM\" +2W}</b>.</li> <li style=\"margin-left:15px\">Finally, please email me when you are finished.</li> </ol> <p>Thanks for all your efforts!</p> <div dir=\"ltr\" class=\"gmail_signature\" data-lt-sig=\"1\">  <span style=\"color:blue;\">John Costanzo <br>Henderson County Democrats Postcard Drive Coordinator</span>  </div> <br clear=\"all\">",
         ";pcfu": "<style type=\"text/css\">div, li, p {font-size:large;}</style><span style=\"color:rgb(0,0,0);font-family:Georgia,serif\">I\'m checking whether this is something you\'d be interested in doing.<p>If not, I\'d be happy to remove your name from the list.<p><p>Thanks.<p><p><font color=\\\"#0000ff\\\">— jc</font><p></span>",
         ";pcr": "<style type=\"text/css\">div, li, p {font-size:large;}</style>Henderson County Democrats postcard drive reminder<p>As a reminder, we were looking for your assigned postcards to be sent out by the end of <span style=\"color:red; font-weight: bold;\">${DATE \"MMMM\"}</span> and to email me when you have completed this.<p><p>If you are having difficulties sending them out, please let me know.<p><p>And thanks for all you do!",
         ";pig": "<img src=\"https://em-content.zobj.net/source/skype/289/pig_1f416.png\" width=\"66\" height=\"66\" alt=\"Pig on Skype Emoticons 1.2\"/>",
@@ -124,16 +123,15 @@
         ";visit": "cphaguardhouse@gmail.com<style type=\"text/css\">div, li, p {font-size:large;}</style><br>VISITOR to visit the Costanzos at 25 Bay Magnolia Court<br>John and Tracey Costanzo expect <b>VISITORS</b> to visit us at 25 Bay Magnolia Court on <b>DATE</b>.",
         ";whateva": "¯\\\\_(ツ)_/¯",
         ";wink": "<img src=\"https://em-content.zobj.net/source/noto-emoji-animations/344/winking-face_1f609.gif\" width=\"42\" height=\"42\"  alt=\"Winking Face on Noto Color Emoji, Animated 14.0\"/>",
-        ";wir": "When I run this, I receive the error \"\".",
         ";woohoo": "<span style=\"font-size: x-large;\">🎉🙌🥳</span>",
         ";wow": "<img src=\"https://em-content.zobj.net/source/noto-emoji-animations/344/exploding-head_1f92f.gif\" width=\"42\" height=\"42\" alt=\"Exploding Head on Noto Color Emoji, Animated 14.0\"/>",
         ";xx": "Smiling Face with Smiling Eyes on Noto Color Emoji, Animated 14.0",
         ";yum": "<img src=\"https://em-content.zobj.net/source/noto-emoji-animations/344/face-savoring-food_1f60b.gif\" width=\"42\" height=\"42\" alt=\"Face Savoring Food on Noto Color Emoji, Animated 14.0\"/>",
         "<-": "←",
-        "<<<<": "❮❮❮❮",
+        "<<<": "❮❮❮",
         "<=": "⇦",
         "=>": "⇨",
-        ">>>>": "❯❯❯❯",
+        ">>>": "❯❯❯",
         "@bm": "Bonny Marsh",
         "@dh": "Doreen Herina",
         "@dm": "Dan Mahoney",
@@ -207,7 +205,6 @@
         "wehn": "when",
         "wehther": "whether",
         "wel ": "we\'ll",
-        "wer ": "we\'re",
         "werent": "weren\'t",
         "weve": "we\'ve",
         "whats": "what\'s",
